e255c155fd07e20e361539895b2917f09b53929d
denghe <denghe@hotmail.com>
2020-08-22 01:23:06 +0800

    add test7 for sol. writing...

---
+++ test7/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <sol/sol.hpp>
+#include <cassert>
+
+int main() {
+    sol::state lua;
+    int x = 0;
+    lua.set_function("beep", [&x]{ return ++x; });
+    lua.script("beep()");
+    sol::function a_function = lua["beep"];
+    int v = a_function();
+    std::function<int()> a_std_function = a_function;
+    int v2 = a_std_function();
+    assert(x == 1);
+}
