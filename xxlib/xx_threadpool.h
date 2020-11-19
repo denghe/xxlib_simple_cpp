167bc630dc803139f8dfa7fc97eae3b1646c0518
denghe <denghe@hotmail.com>
2020-11-19 15:23:35 +0800

    minor improve. add mysql multi thread test

--- xxlib/xx_threadpool.h
+++ xxlib/xx_threadpool.h
@@ -10,89 +10,9 @@
 
 namespace xx {
 
-    /*
-    T sample:
-    struct Foo {
-        Foo() = default;
-        void operator()(std::function<void(Foo&)>& job)
-        {
-            try {
-                job(*this);
-            }
-            catch (int const& e) {
-                xx::CoutN("catch throw int: ", e);
-            }
-            catch (std::exception const& e) {
-                xx::CoutN("catch throw std::exception: ", e);
-            }
-            catch (...) {
-                xx::CoutN("catch ...");
-            }
-        }
-    };
-    */
-
-    template<typename T>
+    // 常规线程池
+    template<typename JT = std::function<void()>>
     class ThreadPool {
-        using JT = std::function<void(T &)>;
-
-        std::vector<std::thread> threads;
-        std::queue<JT> jobs;
-        std::mutex mtx;
-        std::condition_variable cond;
-        bool stop = false;
-
-    public:
-        explicit ThreadPool(int const &numThreads = 4) {
-            for (int i = 0; i < numThreads; ++i) {
-                threads.emplace_back([this] {
-                    // require default constructor
-                    T t;
-                    while (true) {
-                        JT job;
-                        {
-                            std::unique_lock<std::mutex> lock(this->mtx);
-                            this->cond.wait(lock, [this] {
-                                return this->stop || !this->jobs.empty();
-                            });
-                            if (this->stop && this->jobs.empty()) return;
-                            job = std::move(this->jobs.front());
-                            this->jobs.pop();
-                        }
-                        // require void operator()(std::function<void(T&)>& job) { job(*this); }
-                        t(job);
-                    }
-                });
-            }
-        }
-
-        int Add(JT &&job) {
-            {
-                std::unique_lock<std::mutex> lock(mtx);
-                if (stop) return -1;
-                jobs.emplace(std::move(job));
-            }
-            cond.notify_one();
-            return 0;
-        }
-
-        ~ThreadPool() {
-            {
-                std::unique_lock<std::mutex> lock(mtx);
-                stop = true;
-            }
-            cond.notify_all();
-            for (std::thread &worker : threads) {
-                worker.join();
-            }
-        }
-    };
-
-    // 从上面复制小改的精简版
-    template<>
-    class ThreadPool<void> {
-        using JT = std::function<void()>;
-
         std::vector<std::thread> threads;
         std::queue<JT> jobs;
         std::mutex mtx;
@@ -141,4 +61,92 @@ namespace xx {
             }
         }
     };
+
+
+    // 带执行环境的版本
+    template<typename Env, size_t numThreads>
+    struct ThreadPool2 {
+        std::vector<Env> envs;
+    protected:
+        using JT = std::function<void(Env&)>;
+        std::vector<std::thread> threads;
+        std::queue<JT> jobs;
+        std::mutex mtx;
+        std::condition_variable cond;
+        bool stop = false;
+
+    public:
+        explicit ThreadPool2() {
+            envs.resize(numThreads);
+            for (int i = 0; i < numThreads; ++i) {
+                threads.emplace_back([this, i=i] {
+                    auto& t = envs[i];
+                    while (true) {
+                        JT job;
+                        {
+                            std::unique_lock<std::mutex> lock(this->mtx);
+                            this->cond.wait(lock, [this] {
+                                return this->stop || !this->jobs.empty();
+                            });
+                            if (this->stop && this->jobs.empty()) return;
+                            job = std::move(this->jobs.front());
+                            this->jobs.pop();
+                        }
+                        t(job);
+                    }
+                    });
+            }
+        }
+
+        int Add(JT&& job) {
+            {
+                std::unique_lock<std::mutex> lock(mtx);
+                if (stop) return -1;
+                jobs.emplace(std::move(job));
+            }
+            cond.notify_one();
+            return 0;
+        }
+
+        ~ThreadPool2() {
+            {
+                std::unique_lock<std::mutex> lock(mtx);
+                stop = true;
+            }
+            cond.notify_all();
+            for (std::thread& worker : threads) {
+                worker.join();
+            }
+        }
+    };
+
+    /*
+    struct Env {
+        // ...
+
+        void operator()(std::function<void(Env&)>& job) {
+            try {
+                job(*this);
+            }
+            catch (int const& e) {
+                xx::CoutN("catch throw int: ", e);
+            }
+            catch (std::exception const& e) {
+                xx::CoutN("catch throw std::exception: ", e);
+            }
+            catch (...) {
+                xx::CoutN("catch ...");
+            }
+        }
+    };
+
+    xx::ThreadPool2<Env, 4> tp;
+
+    std::array<xx::ThreadPool2<Env, 1>, 4> tps;
+
+    tp.Add([](Env& e) {
+        // e.xxxxx
+    });
+
+    */
 }
